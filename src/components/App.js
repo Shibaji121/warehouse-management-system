1545d363e2287578f5475de9de21bb5d10330436
shiba_1234 <shibajisahu121@gmail.com>
2023-11-04 21:32:46 +0530

    header designed done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Shibaji Sahu</h1>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../Header/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <div className="logo">
+          <img
+            src="https://png.pngtree.com/template/20191008/ourmid/pngtree-modern-warehouse-barn-building-colorful-logo-design-overlapping-style-image_315265.jpg"
+            alt="logo"
+          />
+        </div>
+        <h4>All Ware Houses</h4>
+      </div>
+      <div className="header-middle">
+        <input type="text" placeholder="Search any ware house" />
+        <button type="submit">🔍</button>
+      </div>
+      <div className="header-right">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/9408/9408175.png"
+          alt="profile-img"
+        />
+        <h4>Log in</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
