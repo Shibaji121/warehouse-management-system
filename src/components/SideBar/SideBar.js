58f1f1a0ac226f4e362615ead9b7431aaef67d86
shiba_1234 <shibajisahu121@gmail.com>
2023-11-08 00:00:56 +0530

    dynamic data displayed in sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,29 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../SideBar/SideBar.css";
+import { useSelector } from "react-redux";
 
 const SideBar = () => {
   const [rangeValue, setRangeValue] = useState(5000);
+  const [cityList, setCityList] = useState([]);
+  const [cluster, setCluster] = useState([]);
+  const wareHouses = useSelector((state) => state.wareHouse.wareHouses);
 
   const handleRangeChange = (e) => {
     setRangeValue(e.target.value);
   };
+  const handleCitySelect = (e) => {
+    console.log(e.target.value);
+  };
+  const handleClusterSelect = (e) => {
+    console.log(e.target.value);
+  };
+
+  useEffect(() => {
+    const cities = Array.from(new Set(wareHouses.map((house) => house.city)));
+    const cluster = Array.from(
+      new Set(wareHouses.map((house) => house.cluster))
+    );
+    setCityList(cities);
+    setCluster(cluster);
+  }, [wareHouses]);
   return (
     <div className="side-bar-container">
       <div className="side-bar">
         <div>
           <h3>Filter By City</h3>
-          <select>
-            <option>All</option>
-            <option>Delhi</option>
-            <option>Chennai</option>
+          <select onChange={handleCitySelect}>
+            <option value="All">All</option>
+            {cityList.map((city, index) => (
+              <option value={city} key={index}>
+                {city}
+              </option>
+            ))}
           </select>
         </div>
         <div>
           <h3>Filter By Cluster</h3>
-          <select>
-            <option>All</option>
-            <option>cluster-a-1</option>
-            <option>cluster-a-32</option>
+          <select onChange={handleClusterSelect}>
+            <option value="All">All</option>
+            {cluster.map((cluster, index) => (
+              <option value={cluster} key={index}>
+                {cluster}
+              </option>
+            ))}
           </select>
         </div>
         <div>
